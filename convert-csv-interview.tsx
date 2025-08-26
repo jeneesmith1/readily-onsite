da533be03b1a47558bab8bd69d67e5a9f30add95
Jenee Smith <jeneecsmith@gmail.com>
2025-08-26 07:57:28 -0700

    interview draft file

---
+++ convert-csv-interview.tsx
@@ -0,0 +1,106 @@
+# interface Destination {
+#     element_index: string
+#     element: string
+#     requirement_index: string
+#     requirement: 
+#     citations
+
+# }
+
+
+# trim rowa -> rowa and b
+# split based on semicolon
+
+# destination_file = "path to file"
+# source_file = path
+
+# for row in destination_file {
+#     find element_index_row = regex(as a:)
+#     for a, b in rowa {
+#         rowa.split(a, b ":")
+#     }
+#     let rowb = element
+
+#     destination = [ rowa, rowb, rowx ]
+#     desintaiton_rfle.append(destination)
+# }
+
+
+
+# begin at the beginnof of gile
+
+# for active sheet - 
+
+# for row in active_sheet {
+#     for columbn b
+#     // find the first element row
+#     // look for first instance that says "Element letter : with refe" 
+#         then begin for loop or another function
+#             inside that parse the element index from elemtn using : as the delimiter to split 
+#             write helper funtion return elemntindex, element as dict, object
+#     // fkr element iterate through row and find the element index
+#         case eliminate contracted/reluatory
+#         stop function condition is blank row
+#         for every row that is left add to citations
+#         return citations
+#     for column b 
+#         // inside row look for first instance of Audit Requirement
+#             trim based on first instance of .
+#             split into requirement index and requirement
+#             return requirement index and requirement
+
+#     stop everything based on sheet.lastrow
+#     destination = [ element_index, element, requirement_index, requirement, citations ]
+# }
+
+
+interface Destination {
+    element_index: string
+    element: string
+    requirement_index: string
+    requirement: string
+    citations: string
+}
+
+// find active sheet
+
+//find first row
+
+function find_first_row(sheet) {
+    for row in sheet {
+        const regexforRow = /[A-Z]/:
+        // double check for spaces later
+        return index for first row
+        for sheet[row] {
+            
+        }
+        }
+}
+
+function elementandindex {
+            // get element_index and row
+        if row.regex(regexforRow) {
+            for index, element in row {
+                let index, element in row.trim(":")
+                //store in object
+            }
+}
+return elementindex, element
+
+}
+
+function 
+
+// find first instance of element index and element
+
+
+function fin
+
+
+
+
+    
+
+
+
+
